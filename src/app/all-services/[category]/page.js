0c9f05f79264d5d9ab8c6a188aa0ccc3c2b196bb
Arka <arkakarmakar85@gmail.com>
2023-06-21 12:32:52 +0530

    lat long

--- src/app/all-services/[category]/page.js
+++ src/app/all-services/[category]/page.js
@@ -20,7 +20,8 @@ const allServices = ({ params }) => {
                                     <h3><a href="detail.html">Da Alfredo</a></h3>
                                     <small>27 Old Gloucester St</small>
                                     <p>Id placerat tacimates definitionem sea, prima quidam vim no. Duo nobis persecuti cu.</p>
-                                    <a className="address" href="https://www.google.com/maps/dir//Assistance+%E2%80%93+H%C3%B4pitaux+De+Paris,+3+Avenue+Victoria,+75004+Paris,+Francia/@48.8606548,2.3348734,14z/data=!4m15!1m6!3m5!1s0x47e66e1de36f4147:0xb6615b4092e0351f!2sAssistance+Publique+-+H%C3%B4pitaux+de+Paris+(AP-HP)+-+Si%C3%A8ge!8m2!3d48.8568376!4d2.3504305!4m7!1m0!1m5!1m1!1s0x47e67031f8c20147:0xa6a9af76b1e2d899!2m2!1d2.3504327!2d48.8568361" target="_blank">Get directions</a>
+                                    {/* এখানে lat & long  টা স্ট্যাটিক দেয়া আছে । api থেকে ভ্যালু নিয়ে চেঞ্জ করে দিতে হবে। */}
+                                    <a className="address" href={"https://www.google.com/maps/dir/?api=1&destination="+34.059808+","+-118.368152} target="_blank">Get directions</a>
                                 </div>
                                 <ul>
                                     <li><span className="loc_open">Now Open</span></li>
