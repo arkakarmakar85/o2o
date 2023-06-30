9d536781d748778cd0f00c34084147ed3b8e3375
esp8266esp8266 <bikikarmakar@gmail.com>
2023-06-30 19:48:11 +0530

    OTP and Phone no 1st stage Complite

--- src/app/auth/page.js
+++ src/app/auth/page.js
@@ -1,6 +1,60 @@
+"use client";
 import FrontLayout from '../components/FrontLayout';
-
+import Link from "next/link";
+//import OpenApi from "./components/OpenApi";
+import OpenApi from "../components/OpenApi";
+import { useEffect, useState } from "react";
 const auth = () => {
+    const [phoneno, setPhoneno] = useState();
+    const [phonenoreply, setPhonenoreply] = useState();
+    const [otp, setOtp] = useState();
+    const [otpreply, setOtpreply] = useState();
+    
+
+    const handleClickPhoneno = async () => {
+     // setSelectedCategory(val); //setting the text value 
+      try {
+        let categories = await OpenApi.get("/send-otp?phone=" + phoneno);
+        alert(categories.data.msg)
+        setPhonenoreply(categories.data.msg);
+      } catch (error) {
+        setPhonenoreply(error);
+        alert(error)
+        console.log({ error });
+      }
+    };
+
+
+
+
+
+    const handleClickOTP = async () => {
+      // setSelectedCategory(val); //setting the text value 
+       try {
+        let otpreply = await OpenApi.get("/verify-otp?phone=" + phoneno+"&otp="+otp);
+         alert(otpreply.data.msg)
+         setOtpreply(otpreply.data.msg);
+       } catch (error) {
+        setOtpreply(error);
+         alert(error)
+         console.log({ error });
+       }
+     };
+ 
+
+
+    const getPlaces = async (val) => {
+        setSelectedPlace(val)
+        try {
+          let places = await OpenApi.get("/get-places?place=" + val);
+          setPlaces(places.data.places);
+        } catch (error) {
+          setPlaces([]);
+          console.log({ error });
+        }
+      };
+
+
     return (
         <FrontLayout>
 
@@ -11,25 +65,66 @@ const auth = () => {
                             <div className="box_account">
                                 <h3 className="client">Login Or Register through your phone</h3>
                                 <div className="form_container">
-                                    
-                                    <div className="form-group">
-                                        <input type="number" className="form-control" name="phone" id="phone" placeholder="Phone*" value="" />
-                                    </div>
-                                    <div className="float-right"><a id="forgot" href="#"><strong>Send OTP</strong></a></div>
+      <div className="form-group">
+        <input
+          type="number"
+          className="form-control"
+          name="phone"
+          id="phone"
+          placeholder="Phone*"
+          value={phoneno}
+          onChange={(e) => setPhoneno(e.target.value)}
+        />
+      </div>
+      
+      <div  onClick={handleClickPhoneno} style={{ textAlign: 'right' }}
+> 
 
-                                    <div className="form-group">
-                                        <input type="password" className="form-control" name="otp" id="otp" defaultValue placeholder="OTP*" value="" />
-                                    </div>
-                                    
-                                    <div className="text-center"><input type="submit" defaultValue="Log In" className="btn_1 full-width" /></div>
-                                    <div id="forgot_pw">
-                                        <div className="form-group">
-                                            <input type="email" className="form-control" name="email_forgot" id="email_forgot" placeholder="Type your email" />
-                                        </div>
-                                        <p>A new password will be sent shortly.</p>
-                                        <div className="text-center"><input type="submit" defaultValue="Reset Password" className="btn_1" /></div>
-                                    </div>
-                                </div>
+<strong>Send OTP</strong>
+
+</div>
+      {/* <div className="float-right">
+        <a id="forgot" href="#">
+          <strong>Send OTP</strong>
+        </a>
+      </div> */}
+
+      <div className="form-group">
+        <input
+          type="password"
+          className="form-control"
+          name="otp"
+          id="otp"
+          placeholder="OTP*"
+          value={otp}
+          onChange={(e) => setOtp(e.target.value)}
+        />
+      </div>
+
+      <div className="text-center">
+      <input
+        type="submit"
+        value="Log In"
+        className="btn_1 full-width"
+        onClick={handleClickOTP}
+      />
+    </div>
+      <div id="forgot_pw">
+        <div className="form-group">
+          <input
+            type="email"
+            className="form-control"
+            name="email_forgot"
+            id="email_forgot"
+            placeholder="Type your email"
+          />
+        </div>
+        <p>A new password will be sent shortly.</p>
+        <div className="text-center">
+          <input type="submit" value="Reset Password" className="btn_1" />
+        </div>
+      </div>
+    </div>
                                 {/* /form_container */}
                             </div>
                             {/* /box_account */}
